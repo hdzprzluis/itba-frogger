7e4ae1c30a9b84a6e3f6fd0f410d754b14822224
nicospuri <nicospuri@b4701c55-b8d4-06df-aa43-4cd09944db53>
2011-08-11 02:22:25 +0000

    Se agregan los nuevos modelos. Faltan detalles sobre los prefabs. No esta creado todavia pero hay que definir unas cosas.

---
+++ trunk/Frogger/Assets/Scripts/MotorcycleMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class MotorcycleMovement : MonoBehaviour {
+	
+	private float speed;
+	
+	private bool collision = false;
+	
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(100, 200);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (!collision)
+		{
+			rigidbody.velocity = speed * transform.forward;
+			//transform.Translate(speed * new Vector3(0,0,1) * Time.deltaTime);	
+		}
+	}
+	
+	void OnCollisionEnter(Collision ci)
+	{
+		Debug.Log(string.Format("rv: {0}", ci.relativeVelocity));
+		if (ci.collider.tag == "car" || ci.collider.tag == "motorcycle" || ci.collider.tag == "truck")
+		{
+			collision = true;
+			rigidbody.AddForce(ci.relativeVelocity * -100);
+		}
+	}
+}
