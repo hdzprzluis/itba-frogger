373705c0a6a14b304806cb2e59714150a8c73007
nicospuri <nicospuri@b4701c55-b8d4-06df-aa43-4cd09944db53>
2011-08-19 03:19:56 +0000

    Se sube el proyecto.

---
+++ Frogger/Assets/Scripts/MovementsManager/VehiculeMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class VehiculeMovement : MonoBehaviour {
+	
+	public float speed;
+	protected bool collision = false;
+	
+	void FixedUpdate()
+	{
+		if( !collision )
+		{
+			rigidbody.velocity = speed * transform.forward;	
+		}
+	}
+	
+	void OnCollisionEnter(Collision ci)
+	{
+		if (ci.collider.tag == "car" || ci.collider.tag == "motorcycle" || ci.collider.tag == "truck")
+		{
+			collision = true;
+			// TODO Detectar si colisiona con la rana
+		}
+	}
+}
