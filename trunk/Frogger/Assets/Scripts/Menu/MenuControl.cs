83d3f7dad1e0b655b9b45aab8a00abd0dbabfabb
gmaldo <gmaldo@b4701c55-b8d4-06df-aa43-4cd09944db53>
2011-08-21 23:54:46 +0000

    guiv3 puri no me mates

---
+++ trunk/Frogger/Assets/Scripts/Menu/MenuControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnMouseEnter() {
+        renderer.material.color = Color.blue;
+    }
+	
+	void OnMouseExit(){
+        renderer.material.color = new Color(255,255,255,255);
+	}
+	void OnMouseDrag(){
+		renderer.material.color = Color.red;
+	}
+}
---
+++ trunk/Frogger/Assets/Scripts/Menu/NewGameControl.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class NewGameControl : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnMouseEnter() {
+        renderer.material.color = Color.blue;
+    }
+	
+	void OnMouseExit(){
+        renderer.material.color = new Color(255,255,255,255);
+	}
+	void OnMouseDrag(){
+		renderer.material.color = Color.red;
+		Application.LoadLevel("Scene1");
+	}		
+}
