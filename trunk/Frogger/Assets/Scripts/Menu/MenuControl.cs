789bad8f459ae67ae6982ba79d2db5e6255ed1a2
gmaldo <gmaldo@b4701c55-b8d4-06df-aa43-4cd09944db53>
2011-08-22 02:34:27 +0000

    boludeces

--- trunk/Frogger/Assets/Scripts/Menu/MenuControl.cs
+++ trunk/Frogger/Assets/Scripts/Menu/MenuControl.cs
@@ -10,16 +10,47 @@ public class MenuControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	}
-	
-	void OnMouseEnter() {
-        renderer.material.color = Color.blue;
-    }
-	
-	void OnMouseExit(){
-        renderer.material.color = new Color(255,255,255,255);
-	}
-	void OnMouseDrag(){
-		renderer.material.color = Color.red;
+		GameObject newgameObject = GameObject.Find("text_NewGame");
+		NewGameControl newGameItem = newgameObject.GetComponent<NewGameControl>();
+		
+		GameObject optionobject = GameObject.Find("Text_OPTION");
+		OptionsControl optionItem = optionobject.GetComponent<OptionsControl>();
+
+		GameObject exitobject = GameObject.Find("text_Exit");
+		ExitControl exitItem = exitobject.GetComponent<ExitControl>();
+		
+		if( Input.GetKeyDown(KeyCode.DownArrow) ){
+			if(!newGameItem.selected && !optionItem.selected && !exitItem.selected){
+				newGameItem.selected  = true;
+			}else if(newGameItem.selected && !optionItem.selected && !exitItem.selected){
+				newGameItem.renderer.material.color = new Color(255,255,255,255);
+				newGameItem.selected  = false;
+				optionItem.selected  = true;
+			}else if(!newGameItem.selected && optionItem.selected && !exitItem.selected){
+				optionItem.renderer.material.color = new Color(255,255,255,255);
+				optionItem.selected  = false;
+				exitItem.selected = true;
+			}else if(!newGameItem.selected && !optionItem.selected && exitItem.selected){
+				exitItem.renderer.material.color = new Color(255,255,255,255);
+				exitItem.selected  = false;
+				newGameItem.selected  = true;
+			}
+		}else if(Input.GetKeyDown(KeyCode.UpArrow)){
+			if(!newGameItem.selected && !optionItem.selected && !exitItem.selected){
+				exitItem.selected = true;
+			}else if(newGameItem.selected && !optionItem.selected && !exitItem.selected){
+				newGameItem.renderer.material.color = new Color(255,255,255,255);
+				newGameItem.selected  = false;
+				exitItem.selected  = true;
+			}else if(!newGameItem.selected && optionItem.selected && !exitItem.selected){
+				optionItem.renderer.material.color = new Color(255,255,255,255);
+				optionItem.selected  = false;
+				newGameItem.selected = true;
+			}else if(!newGameItem.selected && !optionItem.selected && exitItem.selected){
+				exitItem.renderer.material.color = new Color(255,255,255,255);
+				exitItem.selected  = false;
+				optionItem.selected  = true;
+			}
+		}
 	}
 }
