21a919b95d1026c6b54a9a52f722ff736f7cb735
gmaldo <gmaldo@b4701c55-b8d4-06df-aa43-4cd09944db53>
2011-08-23 01:18:35 +0000

    new sounds and minor bug fixes

--- trunk/Frogger/Assets/Scripts/SoundManager.cs
+++ trunk/Frogger/Assets/Scripts/SoundManager.cs
@@ -7,11 +7,15 @@ public class SoundManager : MonoBehaviour {
 	public AudioClip otherHorn;
 	public AudioClip quilombo;
 	public AudioClip gokuDies;
+	public AudioClip gokuWin;
+	public AudioClip background;
 
 	public 
 	// Use this for initialization
 	void Start () {
 		gameObject.AddComponent<AudioSource>();
+		audio.clip = background;
+		audio.Play();
 	}
 	
 	// Update is called once per frame
@@ -19,16 +23,26 @@ public class SoundManager : MonoBehaviour {
 		if(Random.Range(0,1000) < 10){
 			int a = Random.Range(0,4);
 			if( a%4 == 0){
-				audio.PlayOneShot(carClaxon);
+				audio.PlayOneShot(carClaxon,0.2F);
 			}else if( a%4 ==1){
-				audio.PlayOneShot(bikeClaxon);
+				audio.PlayOneShot(bikeClaxon,0.2F);
 			}else if( a%4 ==2){
-				audio.PlayOneShot(otherHorn);
+				audio.PlayOneShot(otherHorn,0.2F);
 			}
 		}
 	}
 	public void PlayGokuDies(){
 		audio.PlayOneShot(gokuDies);
 	}
+	public void PlayGokuWins(){
+		audio.PlayOneShot(gokuWin);
+	}
+	public void VolumeDown(){
+		audio.volume = 0.2F;
+	}
+	public void VolumeUp(){
+		audio.volume = 1F;
+	}
+
 
 }
